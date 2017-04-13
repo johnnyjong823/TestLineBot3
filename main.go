39117cdc65d3d01145154ab531088f8adf88ffcb
johnnyjong823 <johnnyjong823@gmail.com>
2017-04-13 10:13:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 		if _, err := bot.PushMessage("U2c68fd429a99dceccc8956571baa7d00", linebot.NewTextMessage("hello")).Do(); err != nil {
-		txt= txt + err.Error()
+		log.Print(err)
 		}
 		if event.Type == linebot.EventTypeFollow {
 			var text = "Hi!歡迎使用魔物獵人LINE@BOT\n"+
